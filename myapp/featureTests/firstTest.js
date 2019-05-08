beafbe6bcbd83e131146bec273fe4c86772edfeb
domw30 <domw30@gmail.com>
2019-05-08 16:24:23 +0100

    update firstTest title

--- myapp/featureTests/firstTest.js
+++ myapp/featureTests/firstTest.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Demo' : function (browser) {
     browser
       .url('http://localhost:3000')
       .assert.containsText('h1', 'The Polyglot Developer')
