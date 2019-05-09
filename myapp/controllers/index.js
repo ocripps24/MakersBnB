de6dce760f6131a4270e3503a03e01d7eab815ad
domw30 <domw30@gmail.com>
2019-05-09 15:36:11 +0100

    add dashboard and add views

--- myapp/controllers/index.js
+++ myapp/controllers/index.js
@@ -1,6 +1,12 @@
 
-
-
 exports.index = function(req, res, next) {
   res.render('index');
+}
+
+exports.dashboard = function(req, res, next) {
+  res.render('dashboard', {name: req.body.email});
+}
+
+exports.add = function(req, res, next) {
+  res.render('add');
 }
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,5 +6,7 @@ let index = require('../controllers/index');
 /* GET home page. */
 router.get('/', index.index);
 router.post('/', index.index);
+router.post('/dashboard', index.dashboard);
+router.get('/add', index.add);
 
 module.exports = router;
