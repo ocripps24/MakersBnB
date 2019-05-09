f1d1d7e2fb28314a847727c950161377b988e6ed
Oliver Cripps <ollycripps@gmail.com>
2019-05-09 15:31:20 +0100

    add backup signup + login functions

--- myapp/controllers/index.js
+++ myapp/controllers/index.js
@@ -1,6 +1,41 @@
+const models = require('../models')
 
-
+var userid
 
 exports.index = function(req, res, next) {
   res.render('index', { title: 'The Polyglot Developer' });
+}
+
+exports.submit_signup = function(req, res, next) {
+  return models.Users.create({
+    username: req.body.signupusername,
+    email: req.body.signupemail,
+    password: req.body.signuppassword
+  }).then(user => {
+    res.redirect("/")
+  })
+};
+
+exports.check_user = function(req, res, next) {
+  return models.Users.findOne({
+		where : {
+      email: req.body.loginemail,
+      password: req.body.loginpassord
+		}
+	}).then(user => {
+    userid = user.id;
+    console.log(userid)
+    res.redirect('/user/'+ userid);
+  })
+}
+
+exports.show_user = function(req, res, next) {
+
+  return models.Users.findOne({
+		where : {
+      id: userid,
+		}
+	}).then(user => {
+    res.render('user', { username: user.username });
+  })
 }
