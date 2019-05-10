6c3045fd44682167dbaab177b4ac105ccda1717e
Chenbo Yi <yichenbo@outlook.com>
2019-05-10 09:00:32 +0100

    implement signup, login, and add properties

--- myapp/controllers/index.js
+++ myapp/controllers/index.js
@@ -1,13 +1,10 @@
 const models = require('../models')
 
-var userid
+var userid, username
+var properties
 
 exports.index = function(req, res, next) {
   res.render('index');
-}
-
-exports.dashboard = function(req, res, next) {
-  res.render('dashboard', {name: req.body.email});
 }
 
 exports.add = function(req, res, next) {
@@ -16,9 +13,9 @@ exports.add = function(req, res, next) {
 
 exports.submit_signup = function(req, res, next) {
   return models.Users.create({
-    username: req.body.signupusername,
-    email: req.body.signupemail,
-    password: req.body.signuppassword
+    username: req.body.signUpName,
+    email: req.body.signUpEmail,
+    password: req.body.signUpPassword
   }).then(user => {
     res.redirect("/")
   })
@@ -27,26 +24,49 @@ exports.submit_signup = function(req, res, next) {
 exports.check_user = function(req, res, next) {
   return models.Users.findOne({
 		where : {
-      email: req.body.loginemail,
-      password: req.body.loginpassord
+      email: req.body.logInEmail,
+      password: req.body.logInPassword
 		}
 	}).then(user => {
     userid = user.id;
-    console.log(userid)
-    res.redirect('/user/'+ userid);
+    username = user.username;
+    res.redirect('/dashboard');
   })
 }
 
-exports.show_user = function(req, res, next) {
+// exports.show_user = function(req, res, next) {
+//
+//   return models.Users.findOne({
+// 		where : {
+//       id: userid,
+// 		}
+// 	}).then(user => {
+//     res.render('dashboard', { name: user.username });
+//   })
+// }
 
-  return models.Users.findOne({
-		where : {
-      id: userid,
-		}
-	}).then(user => {
-    res.render('user', { username: user.username });
+
+exports.add_property = function(req, res, next) {
+  return models.Properties.create({
+    name: req.body.propertyName,
+    description: req.body.propertyDescription,
+    price: req.body.propertyPrice,
+    user_id: userid
+  }).then(property => {
+    res.redirect("/dashboard")
   })
+};
+
+exports.show_properties = function(req, res, next) {
+	return models.Properties.findAll().then(properties => {
+ 	  res.render('dashboard', {properties: properties, name: username  })
+	})
 }
+
+
+
+
+
 
 
 // exports.update_table = function(req, res, next) {
