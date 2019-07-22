59cef62a43121ff4aceb7e8815a92dc24b175723
Oliver Cripps <ollycripps@gmail.com>
2019-07-22 16:12:00 +0100

    Fix Nightwatch feature tests

--- myapp/featureTests/featureTests.js
+++ myapp/featureTests/featureTests.js
@@ -30,6 +30,12 @@ module.exports = {
 
   'User Story Two - step two: set values for form name/description/price' : function (browser) {
     browser
+      .url('http://localhost:3000/')
+      .setValue('#logInEmail', 'jonesbob@email.com')
+      .setValue('#logInPassword', '****')
+      .click('#logIn')
+      .click('#addListing')
+      .verify.urlEquals('http://localhost:3000/add')
       .setValue('#propertyName', 'barcelona flat')
       .setValue('#propertyDescription', 'sunny barcelona flat')
       .setValue('#propertyPrice', '£25')
@@ -39,7 +45,7 @@ module.exports = {
 
   'User Story Two - step three: user listings contains new listing' : function (browser) {
     browser
-      .verify.containsText('#listings', 'barcelona flat')
+      .verify.containsText('#listings', 'BARCELONA FLAT')
       .end();
   },
 
@@ -54,8 +60,8 @@ module.exports = {
       .setValue('#propertyDescription', 'sunny madrid flat')
       .setValue('#propertyPrice', '£40')
       .click('#create')
-      .verify.containsText('#listings', 'madrid flat')
-      .verify.containsText('#listings', 'barcelona flat')
+      .verify.containsText('#listings', 'MADRID FLAT')
+      .verify.containsText('#listings', 'BARCELONA FLAT')
       .end();
   },
 
@@ -65,7 +71,7 @@ module.exports = {
       .setValue('#logInEmail', 'jonesbob@email.com')
       .setValue('#logInPassword', '****')
       .click('#logIn')
-      .click('#edit')
+      .click('button#edit')
       .verify.urlEquals('http://localhost:3000/edit')
       .verify.containsText('h3', 'barcelona flat')
   }
